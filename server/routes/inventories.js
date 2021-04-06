f6c8db8e77a5204151f98b4859cc9c2575cd13dc
Caleb Cross <arkansashurdler@gmail.com>
2021-04-06 13:36:37 -0400

    Initial create new inventory response

--- server/routes/inventories.js
+++ server/routes/inventories.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const fs = require('fs')
+const { v4: uuidv4 } = require('uuid');
 
 
-const WAREHOUSE_JSON_PATH = '../data/warehouses.json'
-const INVENTORY_JSON_PATH = '../data/inventories.json'
+const WAREHOUSE_JSON_PATH = 'data/warehouses.json'
+const INVENTORY_JSON_PATH = 'data/inventories.json'
 
 /* GET ALL INVENTORY ITEMS */
     router.get("/", (req, res) => {
@@ -21,9 +23,32 @@ const INVENTORY_JSON_PATH = '../data/inventories.json'
     })
 
 /* CREATE A NEW INVENTORY ITEM*/
-    router.post("/create", (req, res) => {
+    router.post("/", (req, res) => {
 
+        let inventoryData = fs.readFileSync(INVENTORY_JSON_PATH)
+        let parsedInventoryData = JSON.parse(inventoryData);
+
+        const { itemName, warehouseName, description, category, status, quantity } = req.body
+
+         const newItem ={
+            "id": uuidv4(),
+            "warehouseID": uuidv4(),
+            "warehouseName": warehouseName,
+            "itemName": itemName,
+            "description": description,
+            "category": category,
+            "status": status,
+            "quantity": quantity
+        } 
+
+        parsedInventoryData.push(newItem)
+
+        const stringInventoryData = JSON.stringify(parsedInventoryData);
+        
+        fs.writeFileSync(INVENTORY_JSON_PATH, stringInventoryData)
+        res.status(200).json(newItem)
     })
+
 /* UPDATE SINGLE INVENTORY ITEM*/
     router.patch("/:itemId", (req, res) => {
 
