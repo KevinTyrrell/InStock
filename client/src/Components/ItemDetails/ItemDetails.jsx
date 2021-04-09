aa350cf35dbb44a3da9469c3f8936c15906fcb55
Caleb Cross <arkansashurdler@gmail.com>
2021-04-09 01:18:24 -0400

    cancel and submit button styling

--- client/src/Components/ItemDetails/ItemDetails.jsx
+++ client/src/Components/ItemDetails/ItemDetails.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import './ItemDetails.scss'
+
 
 export class ItemDetails extends Component {
 
--- client/src/Pages/NewInventory.jsx
+++ client/src/Pages/NewInventory.jsx
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
 import ItemDetails from '../Components/ItemDetails/ItemDetails'
 import ItemAvailability from '../Components/ItemAvailability/ItemAvailability'
+/*images*/
+import ArrowBack from "../InStock Assets/Icons/arrow_back-24px.svg"
 import './NewInventory.scss'
 
 export class NewInventory extends Component {
     render() {
         return (
             <div className="form">
+            <div className="title__container-section">
+                <img className="icon" src={ArrowBack} alt="Back Arrow"/>
                 <h1 className="form__title">Add New Inventory Item</h1>
+                </div>
                 <ItemDetails/>
                 <ItemAvailability/>
                 <div className="button__container">
-                    <button className="button-cancel">Cancel</button>
-                    <button className="button-submit">Add Item</button>
+                    <button className="button button-cancel">Cancel</button>
+                    <button className="button button-submit">Add Item</button>
                 </div>
 
             </div>
